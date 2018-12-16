71cf8b1306de9676ab1b5c22cb2bd0f6cb970c98
kevinten10 <596823919@qq.com>
2018-12-16 13:40:04 +0800

    Todo Kotlin Program

---
+++ app/src/main/java/phone/air/ten/com/air_phone/home/HomeActivity.kt
@@ -0,0 +1,14 @@
+package phone.air.ten.com.air_phone.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import phone.air.ten.com.air_phone.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
